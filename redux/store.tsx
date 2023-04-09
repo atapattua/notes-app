32df2c6740e61736478847edeb4d581e4bf17009
atapattua <aatapattu@hugsintl.com>
2023-04-08 20:57:22 -0700

    add notes reducer to redux store

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+//reducers
+import notesReducer from "./notesSlice";
+
 export const store = configureStore({
     reducer: {
-        
+        notes: notesReducer
     }
 })
